78a4cf8c197e02a8db4cf50d7f9920c0e594e405
Jasurbek <jasurbek@Jasurbeks-MacBook-Pro.local>
2026-09-23 19:28:18 +0900

    fix: MIT Task TS

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,14 @@
-console.log("HEllo WORLD!");
+function getSquareNumbers(array: number[]) {
+    const result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        result.push({
+            number: array[i],
+            square: array[i] * array[i]
+        });
+    }
+
+    return result;
+}
+
+console.log(getSquareNumbers([1, 2, 3]));
